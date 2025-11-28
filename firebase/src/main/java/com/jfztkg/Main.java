72feb87c86cd32af35990c14d99b967e84a1dec3
gongjiawei <1487714568@qq.com>
2025-11-28 22:51:12 +0800

    开始构建firebase框架

---
+++ firebase/src/main/java/com/jfztkg/Main.java
@@ -0,0 +1,12 @@
+package com.jfztkg;
+
+/**
+ * @Author gongjiawei
+ * @Date 2025/11/28 22:49
+ * @注释
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
