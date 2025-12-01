129cb2770a99ecd5c0d9c5c54b3b0d7577d66747
jiawei.gong <1487714568@qq.comn>
2025-12-01 18:25:00 +0800

    新增埋点数据查询

--- firebase/src/main/java/com/jfztkg/FirebaseDemoApplication.java
+++ firebase/src/main/java/com/jfztkg/FirebaseDemoApplication.java
@@ -1,22 +1,10 @@
 package com.jfztkg;
 
-/**
- * @Author gongjiawei
- * @Date 2025/11/28 23:40
- * @注释
- */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Spring Boot 启动类
- * 注意：包名要在你所有代码的最上层，比如：
- * com.example.firebase
- *   ├─ config
- *   ├─ controller
- *   ├─ service
- *   └─ model
- */
+
 @SpringBootApplication
 public class FirebaseDemoApplication {
 
